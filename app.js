dd73ee9491ad05d243e7976c644328c5febec8d5
nicogerman <60201413+nicogerman@users.noreply.github.com>
2023-12-15 10:40:58 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 function formatNumberInput(event) {
-    let input = event.target.value.replace(/[^0-9]/g, ''); // Elimina caracteres no numéricos
-    let number = parseInt(input, 10) / 100; // Convierte a número y divide por 100
+    let input = event.target.value.replace(/[^0-9]/g, '');
+    let number = parseInt(input, 10) / 100;
 
     if (!isNaN(number)) {
-        event.target.value = number.toFixed(2); // Formatea a dos decimales
+        event.target.value = number.toFixed(2);
     } else {
-        event.target.value = '0.00'; // Valor por defecto si no es un número
+        event.target.value = '0.00';
     }
 }
 
-['originalPrice', 'shippingCost', 'originalTax', 'newPrice'].forEach(id => {
-    document.getElementById(id).addEventListener('input', formatNumberInput);
-});
-
-document.getElementById('priceForm').addEventListener('submit', function(e){
-    e.preventDefault();
-
+function calculateAndUpdateResults() {
     let originalPrice = parseFloat(document.getElementById('originalPrice').value) || 0;
     let shippingCost = parseFloat(document.getElementById('shippingCost').value) || 0;
     let originalTax = parseFloat(document.getElementById('originalTax').value) || 0;
@@ -27,30 +21,26 @@ document.getElementById('priceForm').addEventListener('submit', function(e){
 
     document.getElementById('result').innerHTML = `
         <div class="alert alert-info">
-            <div class="result-item">
-                <strong>Nuevo Precio:</strong> $<span id="newPriceResult">${newPrice.toFixed(2)}</span>
-                <button onclick="copyToClipboard('#newPriceResult')">Copiar</button>
-            </div>
-            <div class="result-item">
-                <strong>Costo de Envío:</strong> $<span id="shippingCostResult">${shippingCost.toFixed(2)}</span>
-                <button onclick="copyToClipboard('#shippingCostResult')">Copiar</button>
-            </div>
-            <div class="result-item">
-                <strong>Nuevo Impuesto:</strong> $<span id="newTaxResult">${newTax.toFixed(2)}</span>
-                <button onclick="copyToClipboard('#newTaxResult')">Copiar</button>
-            </div>
-            <div class="result-item">
-                <strong>Total:</strong> $<span id="totalResult">${total.toFixed(2)}</span>
-                <button onclick="copyToClipboard('#totalResult')">Copiar</button>
-            </div>
+            <div class="result-item"><strong>Nuevo Precio:</strong> $<span id="newPriceResult">${newPrice.toFixed(2)}</span><button onclick="copyToClipboard('#newPriceResult')">Copiar</button></div>
+            <div class="result-item"><strong>Costo de Envío:</strong> $<span id="shippingCostResult">${shippingCost.toFixed(2)}</span><button onclick="copyToClipboard('#shippingCostResult')">Copiar</button></div>
+            <div class="result-item"><strong>Nuevo Impuesto:</strong> $<span id="newTaxResult">${newTax.toFixed(2)}</span><button onclick="copyToClipboard('#newTaxResult')">Copiar</button></div>
+            <div class="result-item"><strong>Total:</strong> $<span id="totalResult">${total.toFixed(2)}</span><button onclick="copyToClipboard('#totalResult')">Copiar</button></div>
         </div>`;
-});
+}
 
 function copyToClipboard(elementId) {
     let text = document.querySelector(elementId).innerText;
     navigator.clipboard.writeText(text).then(() => {
-        alert('Texto copiado: ' + text);
+        
     }).catch(err => {
         console.error('Error al copiar al portapapeles: ', err);
     });
 }
+
+// Asegúrate de que el DOM esté completamente cargado antes de añadir los eventos
+document.addEventListener('DOMContentLoaded', () => {
+    ['originalPrice', 'shippingCost', 'originalTax', 'newPrice'].forEach(id => {
+        document.getElementById(id).addEventListener('input', formatNumberInput);
+        document.getElementById(id).addEventListener('input', calculateAndUpdateResults);
+    });
+});
